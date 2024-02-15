3c61d93d132ab929f97ac52a06e6866fb55c88d2
FlyingMinion10 <82108651+FlyingMinion10@users.noreply.github.com>
2024-02-15 14:24:44 -0600

    Add files via upload

---
+++ swereve-horus v1.21 light/src/main/java/frc/robot/commands/LauncherFeederCmd.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.LauncherSubsystem;
+
+
+public class LauncherFeederCmd extends Command { // This Command is a while-pressed Type
+private final LauncherSubsystem launcherSubsystem;
+    private final double feederSpeed;
+
+    public LauncherFeederCmd(LauncherSubsystem launcherSubsystem_i, double feederSpeed) {
+        this.launcherSubsystem = launcherSubsystem_i;
+        this.feederSpeed = feederSpeed;
+        addRequirements(launcherSubsystem);
+    }
+
+
+    @Override
+    public void initialize() {
+        System.out.println("LauncherReloadCmd started!");
+    }
+
+    @Override
+    public void execute() { // Los rollers giran hacia adentro para absorber el aro
+        
+        launcherSubsystem.upper_Rollers(-feederSpeed);
+        launcherSubsystem.lower_Rollers(-feederSpeed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        launcherSubsystem.stopRollers();
+        System.out.println("LauncherReloadCmd ended!");
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}

804bb24f019833902d0618691e27c7381c4c7b01
FlyingMinion10 <82108651+FlyingMinion10@users.noreply.github.com>
2024-02-17 15:24:01 -0600

    Add files via upload

---
+++ Master_Code/src/main/java/frc/robot/Autonomous/AutonomousCentreCommand.java
@@ -0,0 +1,81 @@
+package frc.robot.Autonomous;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.LauncherSubsystem;
+
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.LauncherConstants;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+
+import frc.robot.commands.LauncherActivateCmd;
+import java.util.List;
+
+
+public class AutonomousCentreCommand extends SequentialCommandGroup {
+    private final LauncherSubsystem launcherSubsystem;
+    private final SwerveSubsystem swerveSubsystem ;
+    private static final int kNC = -1;
+
+    public AutonomousCentreCommand(SwerveSubsystem swerveSubsystem_i, LauncherSubsystem launcherSubsystem_i) {
+        // Asume que necesitas el subsistema de swerve para ejecutar la trayectoria
+        this.launcherSubsystem = launcherSubsystem_i;
+        this.swerveSubsystem = swerveSubsystem_i;
+        
+            
+            // Shooter command
+            new LauncherActivateCmd(launcherSubsystem, LauncherConstants.kUpperRoller_Speed1);
+            new InstantCommand(() -> launcherSubsystem.stopRollers());
+                
+            // 1. Create trajectory 
+            TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
+                AutoConstants.kMaxSpeedMetersPerSecond,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                    .setKinematics(DriveConstants.kDriveKinematics);
+
+            // 2. Generate trajectory
+            Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+                new Pose2d(0, 0, new Rotation2d(0)),
+                List.of(
+                    new Translation2d(-0.5*kNC, 0*kNC)),
+                new Pose2d(-1*kNC, 0*kNC, Rotation2d.fromDegrees(0)),
+                trajectoryConfig);
+            
+            // 3. Define PID controllers for tracking trajectory
+            PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+            PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+            ProfiledPIDController thetaController = new ProfiledPIDController(
+                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+            thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+            // 4. Construct command to follow trajectory
+            SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+            trajectory,
+            swerveSubsystem::getPose,
+            DriveConstants.kDriveKinematics,
+            xController,
+            yController,
+            thetaController,
+            swerveSubsystem::setModulesState,
+            swerveSubsystem);
+
+            // 5. Add some init and wrap-up, and return everything
+            new SequentialCommandGroup(
+                new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())),
+                new InstantCommand(() -> swerveSubsystem.stopModules()));
+
+    }
+}
